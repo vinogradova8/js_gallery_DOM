7139bb3358f7d28185cd6f015d7f533324df6eb2
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-05-01 10:29:15 +0300

    make build

---
+++ index.99b78cb7.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-shadow */var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var n=t.target;if(!n.classList.contains("gallery__img"))return!1;e.src=n.parentElement.href});//# sourceMappingURL=index.99b78cb7.js.map
+
+//# sourceMappingURL=index.99b78cb7.js.map
